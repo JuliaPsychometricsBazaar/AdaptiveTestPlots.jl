c7c1c7ec7e0724f91889e5845ff38923911e0f83
Frankie Robertson <frankie@robertson.name>
2023-11-24 18:05:01 +0200

    Move out of ComputerAdaptiveTesting.jl

---
+++ test/aqua.jl
@@ -0,0 +1,5 @@
+using Aqua
+using AdaptiveTestPlots
+
+Aqua.test_all(AdaptiveTestPlots, ambiguities=false)
+Aqua.test_ambiguities([AdaptiveTestPlots])
---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using XUnit
+
+
+@testset runner=ParallelTestRunner() xml_report=true "top" begin
+    @testset "aqua" begin
+        include("./aqua.jl")
+    end
+end

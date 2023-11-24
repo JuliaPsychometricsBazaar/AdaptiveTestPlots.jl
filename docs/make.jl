ba081ed28696879a613406fbc4a56811de839554
Frankie Robertson <frankie@robertson.name>
2023-11-24 18:26:28 +0200

    Add barebone docs

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using AdaptiveTestPlots
+using Documenter
+using Documenter.Remotes: GitHub
+
+format = Documenter.HTML(
+    prettyurls=get(ENV, "CI", "false") == "true",
+    canonical="https://JuliaPsychometricsBazaar.github.io/AdaptiveTestPlots.jl",
+)
+
+makedocs(;
+    modules=[AdaptiveTestPlots],
+    authors="Frankie Robertson",
+    repo = GitHub("JuliaPsychometricsBazaar", "AdaptiveTestPlots.jl"),
+    sitename="AdaptiveTestPlots.jl",
+    format=format,
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JuliaPsychometricsBazaar/AdaptiveTestPlots.jl",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,12 @@
+# AdaptiveTestPlots.jl
+
+This is a package for plotting recordings of CATs (Computerised Adaptive Tests).
+
+```@meta
+CurrentModule = AdaptiveTestPlots
+```
+
+## Index
+
+```@index
+```

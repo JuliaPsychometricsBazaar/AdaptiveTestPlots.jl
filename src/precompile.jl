18cda762c2cd0877a8bf19c7d644058094ced12d
Frankie Robertson <frankie@robertson.name>
2024-10-02 13:44:35 +0300

    Add plot_item_bank to precompiles

--- src/precompile.jl
+++ src/precompile.jl
@@ -6,4 +6,17 @@ using PrecompileTools
         f = Figure()
         LabelledToggleGrid(f, (off_label = "Show", on_label = "Hide", active = true))
     end
+
+    using Random
+    using FittedItemBanks
+    using FittedItemBanks.DummyData
+    model = StdModel4PL()
+    domain = OneDimContinuousDomain()
+    response = BooleanResponse()
+    spec = SimpleItemBankSpec(model, domain, response)
+    item_bank = dummy_item_bank(Random.default_rng(42), spec, 1)
+
+    @compile_workload begin
+        plot_item_bank(item_bank)
+    end
 end

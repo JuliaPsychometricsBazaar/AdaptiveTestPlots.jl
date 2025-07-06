eeeaea6df846f6c46eb796008c3c3facb548aa2f
Frankie Robertson <frankie@robertson.name>
2025-07-06 15:27:53 +0300

    Remove format tests

--- test/format.jl
+++
@@ -1,8 +0,0 @@
-using JuliaFormatter
-using AdaptiveTestPlots
-
-@testset "format" begin
-    dir = pkgdir(AdaptiveTestPlots)
-    @test format(dir * "/src"; overwrite = false)
-    @test format(dir * "/test"; overwrite = false)
-end
